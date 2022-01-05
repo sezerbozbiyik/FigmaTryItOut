ccc8312f40a78eff205b9968b12a23415192ca2a
Sezer Bozbıyık <sezer.bozbiyik@icloud.com>
2022-01-05 16:42:35 +0300

    folder name changed

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import MainContainer from './navigation/MainContainer'
+import MainContainer from './src/MainContainer'
 import * as Font from "expo-font";
 import AppLoading from 'expo-app-loading';
 
